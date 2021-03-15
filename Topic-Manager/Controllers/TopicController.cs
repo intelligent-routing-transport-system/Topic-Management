3b99e86039719068752d75d7cbf87e0b1a7edaf3
Gustavo Lotto Cardoso <gustavo.lotto29@gmail.com>
2021-03-15 19:42:28 -0300

    Add project files.

---
+++ Topic-Manager/Controllers/TopicController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Topic_Manager.Kafka;
+using Topic_Manager.Model;
+using System.Text.Json;
+
+namespace Topic_Manager.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class TopicController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TopicController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateMessage([FromBody] MessageBody message)
+        {
+            var messageSend = "";
+            string kafkaConnectionString = _configuration.GetConnectionString("KafkaConnection");
+            using (var producer = new ProducerMessage<string>(message.Payload, message.TopicName, kafkaConnectionString))
+            {
+                messageSend = await producer.Run();
+                Debug.WriteLine(messageSend);
+            }
+            return Ok(messageSend);
+        }
+    }
+}
---
+++ Topic-Manager/Model/MessageBody.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Topic_Manager.Model
+{
+    public class MessageBody
+    {
+        public string TopicName { get; set; }
+        public string Payload { get; set; }
+        public string Version { get; set; }
+        public DateTime MessageDate { get; set; }
+    }
+}
