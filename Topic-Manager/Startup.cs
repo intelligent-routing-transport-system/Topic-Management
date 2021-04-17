77958f0901cdc9cd5f210b5fb1bdc3d0684f8d38
Gustavo Lotto Cardoso <gustavo.lotto29@gmail.com>
2021-04-17 20:13:21 -0300

    make correction on route http

--- Topic-Manager/Startup.cs
+++ Topic-Manager/Startup.cs
@@ -44,8 +44,6 @@ namespace Topic_Manager
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TopicManager"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
